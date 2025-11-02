e93cb07b947c7b9caf5f4b06639d2d9bbc93089e
brancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2025-11-02 16:00:23 -0300

    Added rate limiting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -16,6 +18,10 @@ import { MetasModule } from './metas/metas.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100,
+    }]),
     DatabaseModule,
     AuthModule,
     UsersModule,
@@ -25,6 +31,12 @@ import { MetasModule } from './metas/metas.module';
     DespesasModule,
     ReceitasModule,
     MetasModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
