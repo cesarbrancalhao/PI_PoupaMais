0731b637ecc6eeefa72cd8133e71206e1ab5937c
Yago Rocha <127157431+yagorocha99@users.noreply.github.com>
2025-11-15 00:48:56 -0300

    Create a function to check the position of the currency

--- client/src/app/terminology/currency.ts
+++ client/src/app/terminology/currency.ts
@@ -1,28 +1,33 @@
 import { Moeda } from "@/types/configs";
 
 interface CurrencyInfo {
-  locale: string;
-  currency: string;
-  symbol: string;
+    locale: string;
+    currency: string;
+    symbol: string;
+    position: "before" | "after";
 }
-
+  
 const currencyMap: Record<Moeda, CurrencyInfo> = {
-  real: {
-    locale: "pt-BR",
-    currency: "BRL",
-    symbol: "R$",
-  },
-  dolar: {
-    locale: "en-US",
-    currency: "USD",
-    symbol: "$",
-  },
-  euro: {
-    locale: "de-DE",
-    currency: "EUR",
-    symbol: "€",
-  }
+    real: {
+        locale: "pt-BR",
+        currency: "BRL",
+        symbol: "R$",
+        position: "before"
+    },
+    dolar: {
+        locale: "en-US",
+        currency: "USD",
+        symbol: "$",
+        position: "before"
+    },
+    euro: {
+        locale: "de-DE",
+        currency: "EUR",
+        symbol: "€",
+        position: "after"
+    }
 };
+  
 
 export function formatCurrency(value: number, moeda: Moeda) {
   const config = currencyMap[moeda];
@@ -36,3 +41,14 @@ export function formatCurrency(value: number, moeda: Moeda) {
 export function getCurrencySymbol(moeda: Moeda) {
   return currencyMap[moeda].symbol;
 }
+
+export function getCurrencyPlaceholder(moeda: Moeda) {
+    const cfg = currencyMap[moeda];
+  
+    if (cfg.position === "before") {
+      return `${cfg.symbol} 0,00`;
+    }
+  
+    return `0,00 ${cfg.symbol}`;
+  }
+  
